baccc2bf20665e2b3acf2de6d9a51e3f4be07009
lifangdi <772167928@qq.com>
2018-08-31 20:59:11 +0800

    2018.8.31

---
+++ my-app/src/TodoList.js
@@ -0,0 +1,69 @@
+import React, { Component, Fragment } from 'react';
+import TodoItem from './TodoItem';
+
+class TodoList extends Component {   //React.Component
+  constructor(props){
+    super(props);
+    this.state={
+      list:[],
+      inputValue:''
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+  handleButtonClick(){
+    this.setState({ 
+       list:[...this.state.list,this.state.inputValue],    //... ES6语法 展开运算符
+       inputValue:''
+    })
+  };
+
+  handleInputChange(e){
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+
+  // handleItemClick(index){
+  //   const list = this.state.list;
+  //   list.splice(index,1);
+  //   this.setState({list});      //ES6中键和值同名，可直接写一个
+  // }
+
+  handleDelete(index){
+    const list = [...this.state.list];
+    list.splice(index,1);
+    this.setState({list}); 
+  }
+
+  getTodoItems (){
+    return (
+      this.state.list.map((item,index) => {
+              return <TodoItem 
+                      delete={this.handleDelete} 
+                      key={index} 
+                      content={item} 
+                      index={index}
+                    />
+            })
+    )
+  }
+
+  render() {
+    return (
+      //包裹多个元素，不添加dom
+      <Fragment>     
+        <div>
+          <input className='inputBox' value={this.state.inputValue} onChange={this.handleInputChange}/>
+          <button style={{background: "pink",color: "#fff"}} onClick={this.handleButtonClick}>add</button>   
+        </div>
+        <ul>
+          {this.getTodoItems()}
+        </ul>
+      </Fragment>
+    );
+  }
+}
+
+export default TodoList;    
---
+++ my-app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoList from './TodoList.js';
+import "./style.css";
+
+ReactDOM.render(<TodoList />, document.getElementById('root'));
